157ab44a5004686a7be7bd4ddcc7926ab2e23e9f
AnnaMariaKot <70895873+AnnaMariaKot@users.noreply.github.com>
2021-02-19 05:34:49 +0100

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import './App.css';
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-       <h1>Weather App</h1>
-       <footer>
-        This project was coded by Anna-Maria Kotskieva and is {""}
-        <a href="https://github.com/AnnaMariaKot/weather-app-react" target="_blank" >
-         open-sourced on GitHub
-        </a>
-       </footer>
-       </div>
+        <Weather />
+        <footer>
+          This project was coded by Anna-Maria Kotskieva and is {""}
+          <a
+            href="https://github.com/AnnaMariaKot/weather-app-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
