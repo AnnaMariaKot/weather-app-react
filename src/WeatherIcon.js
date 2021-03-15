b43cadda7e1b1dca0fc421da8697805741f255e1
AnnaMariaKot <70895873+AnnaMariaKot@users.noreply.github.com>
2021-03-15 03:30:24 +0100

    Added forecast and forecast css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="white"
-      size={65}
+      size={55}
       animate={true}
     />
   );
